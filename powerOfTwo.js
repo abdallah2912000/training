af98681486f32f8a45fc2d0f9ab8f6928b5fb3e4
Abdullah Khaled Badr <107265014+abdallah2912000@users.noreply.github.com>
2024-01-23 10:43:13 +0200

    Update powerOfTwo.js

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -1,10 +1,23 @@
-function isPowerOfTwo(e) {
-  for (let i = 0; i < e; i++) {
-    if ((e / 2) % 2 === 0) {
-      return true;
-    }
-    return e === 2 || e === 1 ? true : false;
-  }
-}
+// function isPowerOfTwo(e) {
+//   for (let i = 0; i < e; i++) {
+//     if ((e / 2) % 2 === 0) {
+//       return true;
+//     }
+//     return e === 2 || e === 1 ? true : false;
+//   }
+// }    Big-O = O(n)-Linear
 
-console.log(isPowerOfTwo(3));
+// function isPowerOfTwo(e) {
+//   return (e & (e - 1)) === 0
+//     ? `${e} is a power of two`
+//     : `${e} is not a power of two`;
+// }       // Big-O = O(1)-Constant
+
+// console.log(isPowerOfTwo(3));
+// console.log(isPowerOfTwo(4));
+// console.log(isPowerOfTwo(5));
+// console.log(isPowerOfTwo(8));
+// console.log(isPowerOfTwo(9));
+// console.log(isPowerOfTwo(2));
+// console.log(isPowerOfTwo(1));
+// console.log(isPowerOfTwo(16));
