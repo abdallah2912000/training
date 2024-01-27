25375177ec663c695f233e3b4afc9ca76457a52b
Abdullah Khaled Badr <107265014+abdallah2912000@users.noreply.github.com>
2024-01-27 19:38:18 +0200

    Cartesian Product Algorithm

---
+++ cartesianProduct.js
@@ -0,0 +1,11 @@
+// function cartesianProduct(arr1, arr2) {
+//   let result = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let index = 0; index < arr2.length; index++) {
+//       result.push([arr1[i], arr2[index]]);
+//     }
+//   }
+//   return result;
+// }
+//Big-O = O(M*N)    M = time of loob 1 ,  N = time of loob 2
+// console.log(cartesianProduct([1, 2], [3, 4, 5]));
