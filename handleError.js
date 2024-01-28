4c1e46122d2bdb41b9efa8aa0446fc7d318203f3
Abdullah Khaled Badr <107265014+abdallah2912000@users.noreply.github.com>
2024-01-28 13:44:47 +0200

    Handle Error

---
+++ handleError.js
@@ -0,0 +1,14 @@
+// try and catch handle run time error
+
+// let json = '{"age": 30}';
+// let user = JSON.parse(json);
+// try {
+//   if (!user.name) {
+//     throw new SyntaxError("Incomplete data from user: No Name!!!");
+//   }
+//   console.log(user.name);
+// } catch (error) {
+//   console.log(`JSON Error :${error}`);
+// } finally {
+//   console.log(`Finally always run ${user.age}`);
+// }
